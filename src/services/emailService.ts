a724d2cd9908653a86ecf0a0adc93bb8a0d46444
Ихсанов Алдияр Менжанович <aldiyarikhsanov@gmail.com>
2025-08-09 12:19:47 +0500

    update

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -1,12 +1,16 @@
 // services/emailService.ts
+import { sendEmail } from "../adapters/emailAdapter";
+
 export class ConsoleEmailService {
   async sendRegistration(email: string, code: string, frontUrl?: string) {
     const url = `${frontUrl ?? "https://somesite.com"}/confirm-email?code=${encodeURIComponent(code)}`;
-    console.log(
-      `<h1>Thank for your registration</h1>
-<p>To finish registration please follow the link below:
-  <a href='${url}'>complete registration</a>
-</p>`,
-    );
+    const html = `
+      <h1>Thank for your registration</h1>
+      <p>To finish registration please follow the link below:
+        <a href="${url}">complete registration</a>
+      </p>
+      <p>Or use this code: <b>${code}</b></p>
+    `;
+    await sendEmail(email, "Registration confirmation", html); // <-- ВАЖНО
   }
 }
--- src/setupTestingRoutes.ts
+++ src/setupTestingRoutes.ts
@@ -1,11 +1,20 @@
+// setupTestingRoutes.ts
 import { Express, Request, Response } from "express";
 import { BlogModel } from "./models/BlogModel";
 import { PostModel } from "./models/PostModel";
 import { UserModel } from "./models/UserModel";
+import { CommentModel } from "./models/CommentModel";
+import { clearOutbox } from "./adapters/emailAdapter";
 
 export const setupTestingRoutes = (app: Express) => {
   app.delete("/testing/all-data", async (_req: Request, res: Response) => {
-    await Promise.all([BlogModel.deleteMany({}), PostModel.deleteMany({}), UserModel.deleteMany({})]);
+    await Promise.all([
+      BlogModel.deleteMany({}),
+      PostModel.deleteMany({}),
+      UserModel.deleteMany({}),
+      CommentModel.deleteMany({}), // <-- добавь
+    ]);
+    clearOutbox(); // <-- очистка «почтового ящика»
     res.sendStatus(204);
   });
 };
